7881ad3d132d4d5c16c27513b0a04f8f6aeb3aae
Jan Schulte <janschulte90@googlemail.com>
2012-11-23 14:55:04 +0100

    added labelclickbehavior

---
+++ NewsStand/UI/Behaviors/LabelClickBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace NewsStand.UI.Behaviors
+{
+    public class LabelClickBehavior : Behavior<Label>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.MouseLeftButtonDown += AssociatedObject_MouseLeftButtonDown;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.MouseLeftButtonDown -= AssociatedObject_MouseLeftButtonDown;
+        }
+
+        void AssociatedObject_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Process.Start(AssociatedObject.Content.ToString());
+        }
+    }
+}
