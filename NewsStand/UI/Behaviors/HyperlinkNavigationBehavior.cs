010e67149a8d25fc2109d5823dd8c7c9c493805a
Jan Schulte <janschulte90@googlemail.com>
2012-11-23 20:09:52 +0100

    added linklabel for article urls

---
+++ NewsStand/UI/Behaviors/HyperlinkNavigationBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Documents;
+using System.Windows.Interactivity;
+
+namespace NewsStand.UI.Behaviors
+{
+    public class HyperlinkNavigationBehavior : Behavior<Hyperlink>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            AssociatedObject.Click += new System.Windows.RoutedEventHandler(AssociatedObject_Click);
+        }
+
+        void AssociatedObject_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Process.Start(AssociatedObject.NavigateUri.ToString());
+        }
+    }
+}
