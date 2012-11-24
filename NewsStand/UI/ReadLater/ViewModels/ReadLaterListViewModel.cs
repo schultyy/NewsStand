ec379fa13b527627d6f4c6f10464ff1031c80260
Jan Schulte <janschulte90@googlemail.com>
2012-11-24 20:15:50 +0100

    extracted view for recommendation from data template to usercontrol

--- NewsStand/UI/ReadLater/ViewModels/ReadLaterListViewModel.cs
+++ NewsStand/UI/ReadLater/ViewModels/ReadLaterListViewModel.cs
@@ -1,11 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using Caliburn.Micro;
+using NewsStand.Model;
 
 namespace NewsStand.UI.ReadLater.ViewModels
 {
-    public class ReadLaterListViewModel
+    public class ReadLaterListViewModel : Screen
     {
+        public override string DisplayName
+        {
+            get
+            {
+                return "Recommendations saved for later reading";
+            }
+            set
+            {
+                base.DisplayName = value;
+            }
+        }
+
+        private ObservableCollection<Recommendation> field;
+
+        public ObservableCollection<Recommendation> Field
+        {
+            get { return field; }
+            set
+            {
+                if (field == value)
+                    return;
+                field = value;
+                NotifyOfPropertyChange(() => Field);
+            }
+        }
     }
 }
