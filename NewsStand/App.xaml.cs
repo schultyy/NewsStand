6215a0e8622c37918b7802a81ee7a62587faf9c1
Jan Schulte <janschulte90@googlemail.com>
2012-11-24 14:24:02 +0100

    added handler for dispatcherunhandledexception

--- NewsStand/App.xaml.cs
+++ NewsStand/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Reflection;
 using System.Windows;
 using Caliburn.Micro;
 using NewsStand.Configuration;
@@ -19,12 +20,23 @@ namespace NewsStand
         {
             base.OnStartup(e);
 
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
             var boot = new AppBootstrapper();
 
             Serializer.CheckFolder();
 
             var windowManager = IoC.Get<IWindowManager>();
             windowManager.ShowDialog(new ShellViewModel());
+        }
+
+        void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            var exc = e.Exception is TargetInvocationException ? e.Exception.InnerException : e.Exception;
+
+            MessageBox.Show(exc.Message);
+
+            e.Handled = true;
         }
     }
 }
